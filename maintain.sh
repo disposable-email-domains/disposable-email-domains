382f158f9ed80641b4eefe05e52fdfda00dc6039
Yiğit Altınay <yigit@altinay.tr>
2026-01-05 02:52:38 +0300

    Use mktemp so we don't write to disk

--- maintain.sh
+++ maintain.sh
@@ -3,9 +3,12 @@
 # Unify locale settings temporarily to make sort produce the same order
 export LC_ALL=C
 
-# Converts uppercase to lowercase, sorts, removes duplicates and removes allowlisted domains
-cat disposable_email_blocklist.conf | tr '[:upper:]' '[:lower:]' | sort -f | uniq -i  > temp.conf
-comm -23 temp.conf allowlist.conf > disposable_email_blocklist.conf
+# Create temporary file to work on
+TMPFILE=`mktemp`
 
-rm temp.conf
+# Converts uppercase to lowercase, sorts, removes duplicates and removes allowlisted domains
+cat disposable_email_blocklist.conf | tr '[:upper:]' '[:lower:]' | sort -f | uniq -i  > $TMPFILE
+comm -23 $TMPFILE allowlist.conf > disposable_email_blocklist.conf
+
+rm $TMPFILE
 echo "Done!"
