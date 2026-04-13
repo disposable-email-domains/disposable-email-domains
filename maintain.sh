3e4974035d5d678ef5d766399eb16ab9dd491204
Martin Cech <emulatorer@gmail.com>
2026-04-13 09:29:05 +0200

    fix erroneous tmpfile removal

--- maintain.sh
+++ maintain.sh
@@ -3,7 +3,11 @@
 # Unify locale settings temporarily to make sort produce the same order
 export LC_ALL=C
 
+TMPFILE=$(mktemp)
+
 # Converts uppercase to lowercase, sorts, removes duplicates
-cat disposable_email_blocklist.conf | tr '[:upper:]' '[:lower:]' | sort -f | uniq -i  > disposable_email_blocklist.conf
+cat disposable_email_blocklist.conf | tr '[:upper:]' '[:lower:]' | sort -f | uniq -i > $TMPFILE
+
+mv $TMPFILE disposable_email_blocklist.conf
 
 echo "Done!"
